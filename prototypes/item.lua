0f61210f2b488654cb09ff1bdca7b91a39d090d8
Josh <116049@trinity.vic.edu.au>
2017-03-04 14:10:25 +1100

    Create item.lua

---
+++ prototypes/item.lua
@@ -0,0 +1,11 @@
+data:extend({
+
+ {
+    type = "item",
+    name = "uncooked-spaghetti",
+    icon = "__spagtorio__/graphics/icon_uncookedspag.png",
+    flags = { "goes-to-main-inventory" },
+    subgroup = "intermediate",
+    place_result="spaghetti",
+    stack_size= 200,
+  }
