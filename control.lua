4644b5d67b08c532a98d3b4d0c1477e37c2973c5
Josh <116049@trinity.vic.edu.au>
2017-03-20 13:35:18 +1100

    Now gives free spaghetti
    You now get 2 uncooked spaghetti on spawn.

--- control.lua
+++ control.lua
@@ -1,1 +1,4 @@
---not used yet
+script.on_event(defines.events.on_player_created, function(event)
+local insertfreespag = game.players[event.player_index].insert
+insertfreespag{name="uncooked-spaghetti", count=2}
+end)
