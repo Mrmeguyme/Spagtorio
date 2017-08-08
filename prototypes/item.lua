67adc204833616200f63a332158518607eff97d6
Josh <116049@trinity.vic.edu.au>
2017-08-08 13:08:01 +1000

    New Update
    Adding an eatable spaghetti and meatballs.

--- prototypes/item.lua
+++ prototypes/item.lua
@@ -15,5 +15,42 @@ data:extend({
 	flags = { "goes-to-main-inventory" },
 	subgroup = "spagtorio-ingredients",
 	stack_size = 200
+  },
+  {
+	type = "capsule",
+    name = "spaghetti-meatballs",
+    icon = "__Spagtorio__/graphics/icons/spag.png",
+    flags = {"goes-to-quickbar"},
+    subgroup = "spagtorio-ingredients",
+	stack_size = 10,
+    capsule_action =
+    {
+      type = "use-on-self",
+      attack_parameters =
+      {
+        type = "projectile",
+        ammo_category = "capsule",
+        cooldown = 30,
+        range = 0,
+        ammo_type =
+        {
+          category = "capsule",
+          target_type = "position",
+          action =
+          {
+            type = "direct",
+            action_delivery =
+            {
+              type = "instant",
+              target_effects =
+              {
+                type = "damage",
+                damage = {type = "physical", amount = -100}
+              }
+            }
+          }
+        }
+      }
+    }
   }
   })
--- prototypes/recipe.lua
+++ prototypes/recipe.lua
@@ -2,11 +2,24 @@ data:extend({
 {
 	type = "recipe",
 	name = "spaghetti",
+	category = "smelting",
 	enabled = "true",
 	ingredients = 
 	{
-		{"uncooked-spaghetti", 2}
+		{"uncooked-spaghetti", 1}
 	},
 	result = "spaghetti"
+},
+
+{
+	type = "recipe",
+	name = "spaghetti-meatballs",
+	category = "crafting",
+	enabled = "true",
+	ingredients = 
+	{
+		{"spaghetti", 5}
+	},
+	result = "spaghetti-meatballs"
 }
 })
